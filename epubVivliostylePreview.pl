e57c7f1c5de06ec82ea8efc418ad4d2520f7ac7c
JunTajima <33832322+JunTajima@users.noreply.github.com>
2019-07-16 11:22:43 +0900

    Update epubVivliostylePreview.pl

--- epubVivliostylePreview.pl
+++ epubVivliostylePreview.pl
@@ -3,6 +3,10 @@ use utf8;
 #Encodeモジュールをインポート
 use Encode qw/encode decode/;
 
+######Vivliostyleで表示する際のCSS追加指定######
+my $addVivliostyleCss = "&renderAllPages=true&userStyle=data:,/*%3Cviewer%3E*/%0Aimg,%20svg%20%7B%20max-inline-size:%20100%25%20!important;%20max-block-size:%20100vb%20!important;%20object-fit:%20contain%20!important;%20%7D%0A/*%3C/viewer%3E*/%0A\@page%20:left%20%7B%0Amargin-right:%2010mm;%0A\@bottom-left%20%7B%0Acontent:%20counter(page);%0Amargin-left:%205pt;%0Amargin-bottom:%206mm;%0Awriting-mode:%20horizontal-tb;%0Afont-weight:%20normal;%0Afont-family:%20serif-ja,%20serif;%0A%7D%0A%7D%0A\@page%20:right%20%7B%0Amargin-left:%2010mm;%0A\@bottom-right%20%7B%0Acontent:%20counter(page);%0Amargin-right:%205pt;%0Amargin-bottom:%206mm;%0Awriting-mode:%20horizontal-tb;%0Afont-weight:%20normal;%0Afont-family:%20serif-ja,%20serif;%0A%7D%0A%7D&spread=true"; #見開き表示、画像表示最適化、ノンブル表示の指定を追加
+#############################################
+
 #vivliostyleパッケージのパスを取得
 my $vivliostyleFolderPath = $ARGV[0];
 $vivliostyleFolderPath = decode('UTF-8', $vivliostyleFolderPath);
@@ -36,5 +40,5 @@ my $delayCommand = "osascript -e 'delay 2'";
 system $delayCommand;
 
 #Vivliostyle Viewer起動
-my $openVivliostyleCmd = 'osascript -e \'tell application "Google Chrome" to open location "http://localhost:8000/' . $vivliostyleFolderName . '/viewer/vivliostyle-viewer.html#b=http://localhost:8000/tmp/' . $epubUnzipFoldername . "&renderAllPages=true\"'";
+my $openVivliostyleCmd = 'osascript -e \'tell application "Google Chrome" to open location "http://localhost:8000/' . $vivliostyleFolderName . '/viewer/vivliostyle-viewer.html#b=http://localhost:8000/tmp/' . $epubUnzipFoldername . $addVivliostyleCss . "\"'";
 system $openVivliostyleCmd;
